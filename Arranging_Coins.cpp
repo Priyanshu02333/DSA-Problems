6c2e2547d9952663c81f27db057e2d32b588bb58
Priyanshu Nautiyal <137296498+Priyanshu02333@users.noreply.github.com>
2024-06-02 20:08:25 +0530

    Add files via upload

---
+++ Arranging_Coins.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int arrangeCoins(int n);
+};
+/**********************************************************************/
+int Solution::arrangeCoins(int n) {
+    int nRows{1}, lastRight;
+    long int sum{};
+    while (sum <= n) {
+       sum = ((long int)nRows*(nRows+1))/2;
+       if (sum <= n) {
+           lastRight = nRows;
+       }
+       ++nRows; 
+    }
+    return lastRight;
+}
+/**********************************************************************/
